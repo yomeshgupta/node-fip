3902541c4cfc7aa42193d028e70aedc21d78ff85
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-05-02 15:02:14 +0530

    Added Express Support

---
+++ index.js
@@ -0,0 +1,71 @@
+module.exports = function (config) {
+    return function(req, res, next) {
+        var mode = config.mode || 'whitelist',
+            proxy = config.proxy || false,
+            whiteIps = config.whiteIps || [],
+            blackIps = config.blackIps || [],
+            message = config.message || 'Unauthorized Access',
+            userIP = false;
+
+        function setUserIP() {
+            if (proxy) { // If Node server is behind Ngnix or Apache or likes
+                userIP = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
+            } else { // If Node is running directly
+                userIP = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+            }
+            if (userIP.substr(0, 7) == "::ffff:") { // Converting IPv6 to IPv4
+                userIP = userIP.substr(7);
+            }
+        }
+
+        function sendErrorResponse() {
+            return res.send({
+                error: true,
+                message: 'Unauthorized Request'
+            });
+        }
+
+        function whitelistProcessing() {
+            if (whiteIps.length > 0 && whiteIps.indexOf(userIP) != -1) {
+                return true;
+            }
+            return false;
+        }
+
+        function blacklistProcessing() {
+            if (blackIps.length === 0 || blackIps.indexOf(userIP) === -1) {
+                return true;
+            }
+            return false;
+        }
+
+        function checkIP() {
+            var response;
+            switch (mode) {
+                case 'whitelist':
+                    response = whitelistProcessing();
+                    break;
+                case 'blacklist':
+                    response = blacklistProcessing();
+                    break;
+                default:
+                    response = true;
+                    break;
+            }
+            return response;
+        }
+
+        function init() {
+            setUserIP();
+            if (!userIP) {
+                return sendErrorResponse();
+            }
+            var response = checkIP();
+            if (!response) {
+                return sendErrorResponse();    
+            }
+            next();
+        }
+        return init();
+    }
+}
